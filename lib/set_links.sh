70ca2c66fcf802de05532156a1d070a8f2da0b5c
chrisdane <cdanek@awi.de>
2019-08-01 16:46:41 +0200

    set_links

--- lib/set_links.sh
+++ lib/set_links.sh
@@ -3,7 +3,9 @@
 echo
 echo "******** run set_links.sh **********"
 home=$(git rev-parse --show-toplevel)
-ln -s $home/lib/.pre-commit $home/.git/hooks/
-ln -s $home/lib/.post-commit $home/.git/hooks/
+echo "ln -s $home/lib/.pre-commit $home/.git/hooks/pre-commit"
+ln -s $home/lib/.pre-commit $home/.git/hooks/pre-commit
+echo "ln -s $home/lib/.post-commit $home/.git/hooks/post-commit"
+ln -s $home/lib/.post-commit $home/.git/hooks/post-commit
 echo "******** finish set_links.sh **********"
 echo
